f93dc19dc5c60ecb6fd5ef28e980b73587e32139
Sakshi.Tikoo <Sakshi.Tikoo@IN2404574W1.ey.net>
2023-02-08 22:50:03 +0530

    Initial commit

---
+++ springFirst/src/main/java/com/practice/springboot/web/springFirst/SpringFirstApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.web.springFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFirstApplication.class, args);
+	}
+
+}
---
+++ springFirst/src/test/java/com/practice/springboot/web/springFirst/SpringFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.web.springFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
